3bc215a69f8245d08c2b48a45c01c84660e6e93a
ct <charleswtiffany@gmail.com>
2025-03-17 23:56:01 -0700

    added txTypes and basic subscription logic

--- cmd/main.go
+++ cmd/main.go
@@ -7,31 +7,44 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
 	"time"
-    "os"
-	"github.com/mr-tron/base58"
+
 	"github.com/gorilla/websocket"
-    "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
+	"github.com/mr-tron/base58"
 )
-
-const solanaWebSocketURL = "wss://mainnet.helius-rpc.com/?api-key="
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-var clientStatus = ClientStatus{
-	clients: make(map[*websocket.Conn]bool),
-}
 
 type ClientStatus struct {
 	clients map[*websocket.Conn]bool
 	mu      sync.Mutex
 }
 
-var HELIUS_KEY string
+type ClientMessage struct {
+	SubscriptionType string `json:"subscriptionType"`
+}
+
+const (
+	divideByLamports   uint64 = 1_000_000_000
+	solanaWebSocketURL string = "wss://mainnet.helius-rpc.com/?api-key="
+)
+
+var (
+	HELIUS_KEY string
+
+	solConn *websocket.Conn
+
+	subscriptionMap map[string][]*websocket.Conn
+
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+	clientStatus = ClientStatus{
+		clients: make(map[*websocket.Conn]bool),
+	}
+)
 
 type Result struct {
 	Context struct {
@@ -63,11 +76,16 @@ func handleClientWebSocket(w http.ResponseWriter, r *http.Request) {
 	clientStatus.mu.Unlock()
 
 	for {
-		_, _, err := conn.ReadMessage()
+		_, msg, err := conn.ReadMessage()
+		var message string
+		err = json.Unmarshal(msg, &message)
 		if err != nil {
 			log.Println("Client disconnected:", err)
 			break
 		}
+		if len(message) == 32 && strings.Contains(message, "pump") {
+			handleSubscribe(conn, message)
+		} 
 	}
 
 	clientStatus.mu.Lock()
@@ -90,8 +108,36 @@ func sendToClients() {
 	}
 }
 
-func createSolanaWSConnection() (*websocket.Conn, error) {
-	conn, r, err := websocket.DefaultDialer.Dial(solanaWebSocketURL + string(HELIUS_KEY), nil)
+func handleSubscribe(clientAddress *websocket.Conn, subscriptionAddress string) {
+
+	subscriptionMap[subscriptionAddress] = append(subscriptionMap[subscriptionAddress], clientAddress)
+
+	err := clientAddress.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("Successfully subscribed to %v", subscriptionAddress)))
+	if err != nil {
+		log.Println("Error sending message to client:", err)
+		clientAddress.Close()
+
+		subscriptionMap[subscriptionAddress] = removeItems(subscriptionMap[subscriptionAddress], clientAddress)
+
+		if len(subscriptionMap[subscriptionAddress]) == 0 {
+			delete(subscriptionMap, subscriptionAddress)
+		}
+	}
+}
+
+func removeItems(arr []*websocket.Conn, value *websocket.Conn) []*websocket.Conn {
+	var newArr []*websocket.Conn
+	for _, v := range arr {
+		if v != value {
+			newArr = append(newArr, v)
+		}
+	}
+	return newArr
+}
+
+func solanaWSConnection() (*websocket.Conn, error) {
+	conn, r, err := websocket.DefaultDialer.Dial(solanaWebSocketURL+string(HELIUS_KEY), nil)
+	solConn = conn
 	if err != nil {
 		return nil, err
 	}
@@ -101,17 +147,17 @@ func createSolanaWSConnection() (*websocket.Conn, error) {
 		return conn.WriteControl(websocket.PongMessage, []byte(appData), time.Now().Add(5*time.Second))
 	})
 
-	return conn, nil
+	return solConn, nil
 }
 
-func subscribeToSolana(conn *websocket.Conn) {
+func solanaLogsSubscriber(conn *websocket.Conn) {
 	subscriptionMessage := map[string]interface{}{
 		"jsonrpc": "2.0",
 		"id":      1,
 		"method":  "logsSubscribe",
 		"params": []interface{}{
 			map[string]interface{}{
-				"mentions": []string{"11111111111111111111111111111111"},
+				"mentions": []string{"6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P"},
 			},
 			map[string]interface{}{
 				"commitment": "finalized",
@@ -131,15 +177,15 @@ func subscribeToSolana(conn *websocket.Conn) {
 	}
 }
 
-func listenForSolanaEvents() {
+func solanaEventListener() {
 	for {
-		conn, err := createSolanaWSConnection()
+		conn, err := solanaWSConnection()
 		if err != nil {
 			log.Println("Error connecting to Solana WebSocket:", err)
 			time.Sleep(5 * time.Second)
 			continue
 		}
-		subscribeToSolana(conn)
+		solanaLogsSubscriber(conn)
 
 		for {
 			_, msg, err := conn.ReadMessage()
@@ -150,10 +196,12 @@ func listenForSolanaEvents() {
 
 			var messageLogs Result
 			if err = json.Unmarshal(msg, &messageLogs); err != nil {
-				log.Println("Not expected data, skipping.")
 				continue
 			}
-			decodeProgramData(messageLogs.Params.Result.Value.Logs)
+			programData := txFilter(messageLogs.Params.Result.Value.Logs)
+			if programData == "" {
+				continue
+			}
 			messageChannel <- string(msg)
 		}
 
@@ -163,71 +211,111 @@ func listenForSolanaEvents() {
 	}
 }
 
-func decodeProgramData(logs []string) {
-	// var tokenCreation bool
+func txFilter(logs []string) string {
+	var mint string
+	var solAmount uint64
+	var destinationAddress string
+	var buyOrSell string
+	var create bool
+	var err error
+
 	for _, logEntry := range logs {
-
-			if !strings.Contains(logEntry, "Program data:") {
-				continue
-			}
-
-			if !strings.Contains(logEntry, "") {
-				continue
-			}
-
-			parts := strings.Split(logEntry, "Program data:")
-			if len(parts) < 2 {
-				continue
-			}
-
-			base64Data := strings.TrimSpace(parts[1])
-			log.Println("Found base64 program data:", base64Data)
-
-			decodedData, err := base64.StdEncoding.DecodeString(base64Data)
-			if err != nil {
-				log.Println("Error decoding base64:", err)
-				continue
-			}
-
-			if len(decodedData) < 72 {
-				log.Printf("Decoded data too short: got %d bytes\n", len(decodedData))
-				continue
-			}
-
-			// Extract the fields from the decoded data
-			amount := binary.LittleEndian.Uint64(decodedData[:8])
-			mintAddressBytes := decodedData[8:40]
-			destinationAddressBytes := decodedData[40:72]
-
-			// Convert to Base58 strings
-			mintAddress := base58.Encode(mintAddressBytes)
-			destinationAddress := base58.Encode(destinationAddressBytes)
-
-			fmt.Printf("Decoded Token Minting Data:\n")
-			fmt.Printf("  Amount: %d\n", amount)
-			fmt.Printf("  Mint Address (Base58): %s\n", mintAddress)
-			fmt.Printf("  Destination Address (Base58): %s\n", destinationAddress)
+		if strings.Contains(logEntry, "Buy") {
+			buyOrSell = "Buy"
 		}
+		if strings.Contains(logEntry, "Sell") {
+			buyOrSell = "Sell"
+		}
+
+		if strings.Contains(logEntry, "InitializeMint") {
+			create = true
+		}
+
+		if mint != "" && buyOrSell != "" && create {
+			break
+		}
+
+		if !strings.Contains(logEntry, "") || !strings.Contains(logEntry, "Program data:") {
+			continue
+		}
+
+		parts := strings.Split(logEntry, "Program data:")
+		if len(parts) < 2 {
+			continue
+		}
+		if solAmount, mint, destinationAddress, err = decodeProgramData(parts[1]); err != nil {
+			return ""
+		}
+	}
+
+	if create && mint != "" {
+		fmt.Printf("\n============================================")
+		fmt.Printf("\n  Amount: %d\n", solAmount)
+		fmt.Printf("  Mint Address (Base58): %s\n", mint)
+		fmt.Printf("  Destination Address (Base58): %s\n", destinationAddress)
+		fmt.Printf("============================================\n")
+		return "Create"
+	} else if buyOrSell == "Buy" && !create && mint != "" && strings.Contains(mint, "pump") {
+		fmt.Printf("\n============================================")
+		fmt.Println("\n", buyOrSell, "\n", mint)
+		fmt.Printf("============================================\n")
+		return buyOrSell
+	} else if buyOrSell == "Sell" && !create && mint != "" && strings.Contains(mint, "pump") {
+		fmt.Printf("\n============================================")
+		fmt.Println("\n", buyOrSell, "\n", mint)
+		fmt.Printf("============================================\n")
+		return buyOrSell
+	}
+
+	return ""
+}
+
+func decodeProgramData(encodedData string) (amount uint64, mint, Destination string, error error) {
+
+	base64Data := strings.TrimSpace(encodedData)
+
+	decodedData, err := base64.StdEncoding.DecodeString(base64Data)
+
+	if err != nil || len(decodedData) < 72 {
+		return 0, "", "", err
+	}
+
+	// Extract the fields from the decoded data
+	mintAddressBytes := decodedData[8:40]
+
+	// Convert to Base58 strings
+	mintAddress := base58.Encode(mintAddressBytes)
+	if !strings.Contains(mintAddress, "pump") {
+		return 0, "", "", err
+	}
+
+	destinationAddressBytes := decodedData[40:72]
+	destinationAddress := base58.Encode(destinationAddressBytes)
+
+	amount = binary.LittleEndian.Uint64(decodedData[:8])
+	solAmount := amount / divideByLamports
+
+	return solAmount, mintAddress, destinationAddress, nil
 }
 
 func main() {
-    if err := godotenv.Load(); err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	PORT := os.Getenv("PORT")
-    HELIUS_KEY = os.Getenv("HELIUS_KEY")
+	HELIUS_KEY = os.Getenv("HELIUS_KEY")
 	if len(HELIUS_KEY) < 8 {
 		log.Println("Missing key")
-        return
-    }
-    if PORT == "" {
-        PORT = "8080"
-    }
+		return
+	}
+	if PORT == "" {
+		PORT = "8080"
+	}
 
 	http.HandleFunc("/ws", handleClientWebSocket)
 
-	go listenForSolanaEvents()
+	go solanaEventListener()
 	go sendToClients()
 
 	log.Println("WebSocket server listening on :8080...")
